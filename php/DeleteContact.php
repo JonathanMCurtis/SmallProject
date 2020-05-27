1b9100d6afd3aa9a2774471148c868897d4d5d20
Jonathan Curtis <36046310+JonathanMCurtis@users.noreply.github.com>
2020-05-27 16:49:07 -0400

    Update DeleteContact.php
    Updating PHP files to connect using stored credentials.
    Minor change to formatting.

--- php/DeleteContact.php
+++ php/DeleteContact.php
@@ -1,46 +1,48 @@
 <?php
-  // Creates contact in the database
+	// Creates contact in the database
 
-  // Gets the information provided in the file call
-  $inputData = getRequestInfo();
+	// Gets the information provided in the file call
+	$inputData = getRequestInfo();
 
-  // Data to be inserted into database
-  $ID = $inputData["ID"];
-  $FirstName = "";
-  $LastName = "";
-  $Email = "";
-  $Phone = "";
-  $CreationDate = "";
-  $User = "";
+	// Data to be inserted into database
+	$ID = $inputData["ID"];
+	$FirstName = "";
+	$LastName = "";
+	$Email = "";
+	$Phone = "";
+	$CreationDate = "";
+	$User = "";
 
-  // Opens an SQL connection to the database using the credentials below
-  $connection = new mysqli("localhost", "smallProject", "thisIsInsecure", "SmallProjectDB");
-
-  // If there is an error in the connection, returns a formatted error
-  if($connection->connect_error)
-  {
-    returnWithError($connection->connect_error);
-  }
-  else
-  {
-    // Create an SQL statement to insert the provided data into the contacts
-    // table.
-    $sql = "DELETE FROM Contacts WHERE ID = " . $ID;
-
-    $connection->query($sql);
-  }
-
-  // Close the connection
-  $connection->close();
+	// Opens an SQL connection to the database using the stored credentials
+	$ini = parse_ini_file("../../php/temp.ini");
+	$connection = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
 
 
+	// If there is an error in the connection, returns a formatted error
+	if($connection->connect_error)
+	{
+		returnWithError($connection->connect_error);
+	}
+	else
+	{
+		// Create an SQL statement to insert the provided data into the contacts
+		// table.
+		$sql = "DELETE FROM Contacts WHERE ID = " . $ID;
 
-  function getRequestInfo()
+		$connection->query($sql);
+	}
+
+	// Close the connection
+	$connection->close();
+
+
+
+	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-  function returnWithError($error)
+	function returnWithError($error)
 	{
 		$retValue = '{"ID":"","FirstName":"", "LastName":"","Email":"", "Phone":"","CreationDate":"", "User":"", "error":"' . $error . '"}';
 		sendResultInfoAsJson($retValue);
