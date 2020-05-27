cca8e5cc59ee112e8665c8106623119e06b5299d
Jonathan Curtis <36046310+JonathanMCurtis@users.noreply.github.com>
2020-05-27 16:55:05 -0400

    Update ReadContact.php
    Updating PHP files to connect via stored credentials.
    Minor changes to formatting.

--- php/ReadContact.php
+++ php/ReadContact.php
@@ -1,71 +1,71 @@
 <?php
-  // Creates contact in the database
+	// Creates contact in the database
 
-  // Gets the information provided in the file call
-  $inputData = getRequestInfo();
+	// Gets the information provided in the file call
+	$inputData = getRequestInfo();
 
-  // Data to be inserted into database
-  $ID = "";
-  $FirstName = "";
-  $LastName = "";
-  $Email = "";
-  $Phone = "";
-  $CreationDate = "";
-  $User = $inputData[User];
+	// Data to be inserted into database
+	$ID = "";
+	$FirstName = "";
+	$LastName = "";
+	$Email = "";
+	$Phone = "";
+	$CreationDate = "";
+	$User = $inputData[User];
 
-  // Opens an SQL connection to the database using the credentials below
-  $connection = new mysqli("localhost", "smallProject", "thisIsInsecure", "SmallProjectDB");
+	// Opens an SQL connection to the database using the stored credentials
+	$ini = parse_ini_file("../../php/temp.ini");
+	$connection = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
 
-  // If there is an error in the connection, returns a formatted error
-  if($connection->connect_error)
-  {
-    returnWithError($connection->connect_error);
-  }
-  else
-  {
-      // Create an SQL statement to get data from contacts table from contact
-      // with the provided ID/
-      $sql = "SELECT ID,FirstName,LastName,Email,Phone,CreationDate,User FROM Contacts where ID = " . $User;
+	// If there is an error in the connection, returns a formatted error
+	if($connection->connect_error)
+	{
+		returnWithError($connection->connect_error);
+	}
+	else
+	{
+		// Create an SQL statement to get data from contacts table from contact
+		// with the provided ID/
+		$sql = "SELECT ID,FirstName,LastName,Email,Phone,CreationDate,User FROM Contacts where ID = " . $User;
 
-      $result = $connection->query($sql);
+		$result = $connection->query($sql);
 
-      $contacts = array();
+		$contacts = array();
 
-      if($result->num_rows > 0)
-      {
+		if($result->num_rows > 0)
+		{
+			// Get the associated row with this result
+			while($row = result->fetch_assoc())
+			{
+				// Assign values in the row to variables
+				$ID = $row[ID];
+				$FirstName = $row[FirstName];
+				$LastName = $row[LastName];
+				$Email = $row[Email];
+				$Phone = $row[Phone];
+				$CreationDate = $row[CreationDate];
 
-        // Get the associated row with this result
-        while($row = result->fetch_assoc())
-        {
-          // Assign values in the row to variables
-          $ID = $row[ID];
-          $FirstName = $row[FirstName];
-          $LastName = $row[LastName];
-          $Email = $row[Email];
-          $Phone = $row[Phone];
-          $CreationDate = $row[CreationDate];
+				$contacts[] = '{"ID":"'. $ID .'","FirstName":"'. $FirstName .'", "LastName":"'. $LastName .'","Email":"'. $Email .'", "Phone":"'. $Phone .'","CreationDate":"'$CreationDate'", "User":"'. $User .'", "error":"' . $error . '"}';
+			}
 
-          $contacts[] = '{"ID":"'. $ID .'","FirstName":"'. $FirstName .'", "LastName":"'. $LastName .'","Email":"'. $Email .'", "Phone":"'. $Phone .'","CreationDate":"'$CreationDate'", "User":"'. $User .'", "error":"' . $error . '"}';
-        }
+			$connection->close();
+			sendResultInfoAsJson($contacts);
+		}
+		else
+		{
+			// No result exists
+			$connection->close();
+			returnWithError("Contact does not exist");
+		}
 
-        $connection->close();
-        sendResultInfoAsJson($contacts);
-      }
-      else
-      {
-        // No result exists
-        $connection->close();
-        returnWithError("Contact does not exist");
-      }
+	}
 
-  }
-
-  // Close the connection
-  $connection->close();
-
+	// Close the connection
+	$connection->close();
 
 
-  function returnWithError($error)
+
+	function returnWithError($error)
 	{
 		$retValue = '{"ID":"'. $ID .'","FirstName":"'. $FirstName .'", "LastName":"'. $LastName .'","Email":"'. $Email .'", "Phone":"'. $Phone .'","CreationDate":"'$CreationDate'", "User":"'. $User .'", "error":"' . $error . '"}';
 		sendResultInfoAsJson($retValue);
