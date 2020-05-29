5e8190695bd7b2d6a1e3c116b04bbe9972ccae03
Jonathan Curtis <36046310+JonathanMCurtis@users.noreply.github.com>
2020-05-29 17:26:51 -0400

    Update Login.php

--- php/Login.php
+++ php/Login.php
@@ -1,70 +1,46 @@
-<?php
-	// Gets the information provided when the php file was called
-	$inData = getRequestInfo();
+<?php require './Functions.php';
+    // Gets the information provided in the API call
+    $received = file_get_contents('php://input');
+    // Checks whether the login information provided is accurate.
+    $requiredProps = ["User","Password"];
 
-	// Creates new variables to store the data in
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
+    $inputData = getRequestInfo($received);
+    if(ensureProps($inputData, $requiredProps))
+    {
+        $User = $inputData["User"];
+        $Password = $inputData["Password"];
 
-	// Opens an SQL connection to the database using the stored credentials
-	$ini = parse_ini_file("../../php/temp.ini");
-	$connection = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
+        // Opens an SQL connection to the database using the stored credentials
+        $ini = parse_ini_file("../../php/temp.ini");
+        $conn = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
 
-	// If the connection status returns an error, return with a connection error
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
-		$conn->close();
-	}
-	else
-	{
-		// Otherwise, create a SQL statement to get the ID, firstName and lastName from the Users table, where the login and password match the provided info
-		$sql = "SELECT ID,FirstName,LastName FROM Users where User='" . $inData["User"] . "' and Password='" . $inData["Password"] . "'";
-		// Execute this query, and store the results in a new variable called 'result'
-		$result = $conn->query($sql);
-		// If there is at least one row returned
-		if ($result->num_rows > 0)
-		{
-			// Get the associated row with this result
-			$row = $result->fetch_assoc();
-			// Store the first name, last name, and ID from that row
-			$firstName = $row["FirstName"];
-			$lastName = $row["LastName"];
-			$id = $row["ID"];
+        // If the connection status returns an error, return with a connection error
+        if ($conn->connect_error) {
+            returnWithError(503, $conn->connect_error);
+        } else {
+            // Otherwise, check to see if a row matches the provided login information
+            $sql = "SELECT UserID,FirstName,LastName FROM Users where User='" . $User . "' and Password='" . $Password . "'";
+            // Execute this query, and store the results in a new variable called 'result'
+            $result = $conn->query($sql);
+            // If there is at least one row returned
+            if ($result->num_rows > 0) {
+                // Get the associated row with this result
+                $row = $result->fetch_assoc();
+                // Create an array for our JSON output
+                $output = array();
+                $output["ErrorID"] = 0;
+                $output["Error"] = "";
+                $output["UserID"] = $row["UserID"];
+                $output["FirstName"] = $row["FirstName"];
+                $output["LastName"] = $row["LastName"];
 
-			// Return this information from the call
-			returnWithInfo($firstName, $lastName, $id );
-		}
-		else
-		{
-			// Otherwise, no record matching that exists (either user doesn't exist, or invalid password)
-			returnWithError("UserID or Password does not exists");
-		}
-		// Close the connection
-		$conn->close();
-	}
-
-	function returnWithError($error)
-	{
-		$retValue = '{"id":0,"FirstName":"","LastName":"","error":"' . $error . '"}';
-		sendResultInfoAsJson($retValue);
-	}
-
-	function sendResultInfoAsJson($obj)
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-
-	function returnWithInfo($firstName, $lastName, $id)
-	{
-		$retValue = '{"id":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson($retValue);
-	}
-
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-?>
+                // Send this output to be encoded and returned
+                returnWithInfo($output);
+            } else {
+                // Otherwise, no record matching that exists (either user doesn't exist, or invalid password)
+                returnWithError(401, "Incorrect Username or Password");
+            }
+            // Close the connection
+            $conn->close();
+        }
+    }
