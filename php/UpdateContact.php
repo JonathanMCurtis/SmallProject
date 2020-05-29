7a6f95a6bcdc6c45340d4ae56f52e584f9d02d38
Jonathan Curtis <36046310+JonathanMCurtis@users.noreply.github.com>
2020-05-29 17:27:14 -0400

    Update UpdateContact.php

--- php/UpdateContact.php
+++ php/UpdateContact.php
@@ -1,66 +1,42 @@
-<?php
-    // Updates contact in the database
+<?php require './Functions.php';
+    // Updates a contact record in the database
+    $requiredProps = ["ContactID","FirstName", "LastName", "Email", "Phone", "UserID"];
 
-    // Gets the information provided in the file call
-    $inputData = getRequestInfo();
+    // Gets the information provided in the API call
+    $received = file_get_contents('php://input');
+    $inputData = getRequestInfo($received);
+    if(ensureProps($inputData, $requiredProps)) {
 
-    // Data to be inserted into database
-    $ID = $inputData["ID"];
-    $FirstName = $inputData["FirstName"];
-    $LastName = $inputData["LastName"];
-    $Email = $inputData["Email"];
-    $Phone = $inputData["Phone"];
-    $CreationDate = date("d-m-Y");
-    $User = $inputData["User"];
+        // Data to be inserted into database
+        $ContactID = $inputData["ContactID"];
+        $FirstName = $inputData["FirstName"];
+        $LastName = $inputData["LastName"];
+        $Email = $inputData["Email"];
+        $Phone = $inputData["Phone"];
+        $UserID = $inputData["UserID"];
 
-	// Opens an SQL connection to the database using the stored credentials
-	$ini = parse_ini_file("../../php/temp.ini");
-	$connection = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
+        // Opens an SQL connection to the database using the stored credentials
+        $ini = parse_ini_file("../../php/temp.ini");
+        $connection = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
 
-    // If there is an error in the connection, returns a formatted error
-    if($connection->connect_error)
-    {
-        returnWithError($connection->connect_error);
-    }
-    else
-    {
-        $sql = "SELECT ID FROM Contacts WHERE ID = '" . $inputData["ID"] . "' AND User = '" . $inputData["User"] ."'";
+        // If there is an error in the connection, returns a formatted error
+        if ($connection->connect_error) {
+            returnWithError(503, $connection->connect_error);
+        } else {
+            $sql = "SELECT ContactID FROM Contacts WHERE ContactID = '" . $inputData["ContactID"] . "' AND UserID = '" . $inputData["UserID"] . "'";
 
-        $result = $connection->query($sql);
+            $result = $connection->query($sql);
 
-        if($result->num_rows > 0)
-        {
-			// Create an SQL statement to insert the provided data into the contacts
-			// table.
-			$sql = "UPDATE Contacts SET FirstName = '" . $FirstName . "', LastName = '" . $LastName . "', Email = '" . $Email . "', Phone = '" . $Phone . "', CreationDate = '" . $CreationDate . "' WHERE ID = '" . $ID . "'";
+            if (mysqli_num_rows($result) != 0) {
+                $sql = "UPDATE Contacts SET FirstName = '" . $FirstName . "', LastName = '" . $LastName . "', Email = '" . $Email . "', Phone = '" . $Phone . "', CreationDate = '" . $CreationDate . "' WHERE ContactID = '" . $ContactID . "'";
 
-			$connection->query($sql);
+                $connection->query($sql);
+                returnWithInfo(array());
+            } else {
+                returnWithError(204, "This contact does not exist.");
+            }
         }
 
-        else
-        {
-          returnWithError("This contact does not exist.");
-        }
+        // Close the connection
+        $connection->close();
     }
-
-    // Close the connection
-    $connection->close();
-
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
-
-    function returnWithError($error)
-    {
-        $retValue = '{"ID":"","FirstName":"", "LastName":"","Email":"", "Phone":"","CreationDate":"", "User":"", "error":"' . $error . '"}';
-        sendResultInfoAsJson($retValue);
-    }
-
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
-
- ?>
