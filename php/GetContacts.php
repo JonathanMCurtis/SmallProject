73377e02dccf8b412669228c591beab4e573a7cc
Jonathan Curtis <36046310+JonathanMCurtis@users.noreply.github.com>
2020-05-29 17:26:25 -0400

    Update GetContacts.php

--- php/GetContacts.php
+++ php/GetContacts.php
@@ -1,91 +1,33 @@
-<?php
-	// Creates contact in the database
+<?php require './Functions.php';
+    // Gets all contacts associated with a given user.
+    $requiredProps = ["UserID"];
 
-	// Gets the information provided in the file call
-	$inputData = getRequestInfo();
+    // Gets the information provided in the API call
+    $received = file_get_contents('php://input');
+    $inputData = getRequestInfo($received);
+    if(ensureProps($inputData, $requiredProps)) {
+        $UserID = $inputData["UserID"];
 
-	// Data to be inserted into database
-	$ID = "";
-	$FirstName = "";
-	$LastName = "";
-	$Email = "";
-	$Phone = "";
-	$CreationDate = "";
-	$User = $inputData[User];
+        // Opens an SQL connection to the database using the stored credentials
+        $ini = parse_ini_file("../../php/temp.ini");
+        $connection = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
 
-	// Opens an SQL connection to the database using the stored credentials
-	$ini = parse_ini_file("../../php/temp.ini");
-	$connection = new mysqli("localhost", $ini["username"], $ini["password"], $ini["db_name"]);
+        if ($connection->connect_error) {
+            returnWithError(503, $connection->connect_error);
+        } else {
+            $sql =  "SELECT ContactID, FirstName, LastName, Email, Phone, ".
+                    "FROM_UNIXTIME(CreationDate, '%m/%d/%y') as CreationDate ".
+                    "FROM Contacts WHERE UserID = " . $UserID .
+                    " ORDER BY LastName ASC, FirstName ASC";
+            $result = $connection->query($sql);
 
-	// If there is an error in the connection, returns a formatted error
-	if($connection->connect_error)
-	{
-		returnWithError($connection->connect_error);
-	}
-	else
-	{
-		// Create an SQL statement to get data from contacts table from contact
-		// with the provided ID/
-		$sql = "SELECT ID,FirstName,LastName,Email,Phone,CreationDate,User FROM Contacts where ID = " . $User;
-
-		$result = $connection->query($sql);
-
-		$contacts = array();
-
-		if($result->num_rows > 0)
-		{
-			// Get the associated row with this result
-			while($row = result->fetch_assoc())
-			{
-				// Assign values in the row to variables
-				$ID = $row[ID];
-				$FirstName = $row[FirstName];
-				$LastName = $row[LastName];
-				$Email = $row[Email];
-				$Phone = $row[Phone];
-				$CreationDate = $row[CreationDate];
-
-				$contacts[] = '{"ID":"'. $ID .'","FirstName":"'. $FirstName .'", "LastName":"'. $LastName .'","Email":"'. $Email .'", "Phone":"'. $Phone .'","CreationDate":"'$CreationDate'", "User":"'. $User .'", "error":"' . $error . '"}';
-			}
-
-			$connection->close();
-			sendResultInfoAsJson($contacts);
-		}
-		else
-		{
-			// No result exists
-			$connection->close();
-			returnWithError("Contact does not exist");
-		}
-
-	}
-
-	// Close the connection
-	$connection->close();
-
-
-
-	function returnWithError($error)
-	{
-		$retValue = '{"ID":"'. $ID .'","FirstName":"'. $FirstName .'", "LastName":"'. $LastName .'","Email":"'. $Email .'", "Phone":"'. $Phone .'","CreationDate":"'$CreationDate'", "User":"'. $User .'", "error":"' . $error . '"}';
-		sendResultInfoAsJson($retValue);
-	}
-
-	function sendResultInfoAsJson($obj)
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-
-	function returnWithInfo($ID,$FirstName,$LastName,$Email,$Phone,$CreationDate,$User)
-	{
-		$retValue = '{"ID":"'. $ID .'","FirstName":"'. $FirstName .'", "LastName":"'. $LastName .'","Email":"'. $Email .'", "Phone":"'. $Phone .'","CreationDate":"'$CreationDate'", "User":"'. $User .'", "error":""}';
-		sendResultInfoAsJson($retValue);
-	}
-
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
- ?>
+            if (!mysqli_num_rows($result)==0) {
+                $connection->close();
+                returnWithList($result, true);
+            } else {
+                $connection->close();
+                returnWithError(204, "No contacts associated with that User ID.");
+            }
+        }
+        $connection->close();
+    }
