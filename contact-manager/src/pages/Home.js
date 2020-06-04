87deb0c52d3723fce897946a5281d5a0772004a7
idelmr <idel_martinez@knights.ucf.edu>
2020-06-03 22:01:15 -0400

    Add images and style homepage

--- contact-manager/src/pages/Home.js
+++ contact-manager/src/pages/Home.js
@@ -1,9 +1,63 @@
-import React, { Component, Figure } from 'react'; // added Figure for image
-import halfsun from './../data/HalfLogo.png'; // top half of logo
+import React, { Component } from 'react';
+import halfSun from '../data/HalfLogo.png';
+import background from '../data/beach-landing.jpg';
+import curtis from '../data/curtis.jpg';
+import liderma from '../data/liderma.jpg';
+import alex from '../data/alex.jpg';
+import mason from '../data/mason.jpg';
+
 import './styles.css';
+import { Container, Col, Row, Image } from './';
 
 export default class Home extends Component {
-	renderCarousel() {
+	renderHeading() {
+		return (
+			<div className = 'headingContainer'>
+				<div className = 'imgContainer'>
+					<img src = { background } width = '100%' alt = 'SummerTime Contacts' />
+				</div>
+				<div className = 'headerLayer'>
+					<div className = 'header'>
+						<img src = { halfSun } width = '90%' />
+						<h1 className = 'text-dark'>SummerTime Contacts</h1>
+						<h2 className = 'text-dark'>Most revolutionary online Contact Manager</h2>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	renderTeam() {
+		const team = [
+			{ name: 'John Curtis', img: curtis, role: 'Project Manager', link: '' },
+			{ name: 'Liderma Guerry', img: liderma, role: 'Database', link: '' },
+			{ name: 'Alex Varga', img: alex, role: 'API', link: '' },
+			{ name: 'Dakota', img: '', role: 'API', link: '' },
+			{ name: 'Mason', img: mason, role: 'Front-end', link: '' },
+			{ name: 'Idel Martinez', img: '', role: 'Front-end', link: '' }
+		];
+
+		return (
+			<Container>
+				<h1>Team</h1>
+				<Row>
+					{ team.map(({ name, img, role, link }) => {
+						return (
+							<Col className = 'center'>
+								<Image width = '90%' src = { img } rounded />
+								<div className = 'center text-info'>
+									<a href = { link }>{ name }</a>
+									<p>{ role }</p>
+								</div>
+							</Col>
+						);
+					}) }
+				</Row>
+			</Container>
+		);
+	}
+
+	renderReviews() {
 		return (
 			<></>
 		);
@@ -12,7 +66,9 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div>
-				{ this.renderCarousel() }
+				{ this.renderHeading() }
+				{ this.renderTeam() }
+				{ this.renderReviews() }
 			</div>
 		);
 	}
